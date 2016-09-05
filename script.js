ab68db9c110a843a245a2f8b1b42c7237cde62bb
Cody Loyd <codyloyd@gmail.com>
2016-09-05 07:14:48 -0500

    it works.. it sucks.. but it works

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
+var scrollCounter = 1
 $(document).ready(function() {
+  callAJAX(scrollCounter)
+  $(window).scroll(function(event) {
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+       callAJAX(scrollCounter)
+     }
+  });
+});
+
+function callAJAX(page) {
+  scrollCounter += 1
   $.ajax({
-    url: 'http://www.omdbapi.com/?s=tiger&y=&plot=short&r=json&page=1',
+    url: 'http://www.omdbapi.com/?s=tiger&y=&plot=short&r=json&page=' + page,
     type: 'GET',
     dataType: 'json',
     data: {param1: 'value1'}
   })
   .done(function(json) {
     for (var i = 0; i < json.totalResults; i++) {
-      $(".container").append('<p>' + json["Search"][i]["Title"] + '</p>')
-    }
-    console.log("success");
-  })
-  .fail(function() {
-    console.log("error");
-  })
-  .always(function() {
-    console.log("complete");
-  });
-
-});
+      $(".container").append(
+        '<section class="result"> <p>'
+        + json["Search"][i]["Title"] + ' - ' + json["Search"][i]["Year"] +
+        '</p> </section>')
+      }
+    })
+    .fail(function() {
+      console.log("error");
+    })
+  }
